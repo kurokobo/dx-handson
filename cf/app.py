110335943339120fd71a27d0e68fd1cae76d9364
kurokobo <kuro664@gmail.com>
2019-12-20 16:34:43 +0000

    Fix HTML

--- cf/app.py
+++ cf/app.py
@@ -5,31 +5,42 @@ import socket
 import json
 
 # Connect to Redis
-try:    
-    VCAP_SERVICES = json.loads(os.environ['VCAP_SERVICES'])
+try:
+    VCAP_SERVICES = json.loads(os.environ["VCAP_SERVICES"])
     CREDENTIALS = VCAP_SERVICES["rediscloud"][0]["credentials"]
-    redis = Redis(host=CREDENTIALS["hostname"], port=CREDENTIALS["port"], password=CREDENTIALS["password"],
-                  db=0, socket_connect_timeout=2, socket_timeout=2)
+    redis = Redis(
+        host=CREDENTIALS["hostname"],
+        port=CREDENTIALS["port"],
+        password=CREDENTIALS["password"],
+        db=0,
+        socket_connect_timeout=2,
+        socket_timeout=2,
+    )
 
-except:
+except Exception:
     redis = Redis(host="redis", db=0, socket_connect_timeout=2, socket_timeout=2)
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello():
     try:
         visits = redis.incr("counter")
     except RedisError:
-        visits = "<i>cannot connect to Redis, counter disabled</i>"
+        visits = "<i>Cannot connect to Redis, counter disabled</i>"
 
-    html = """<h3>Hello World!</h3><br>
-            Here I am in KL having fun with PKS and Cloud Foundry. AWEEEESSSSOOOOOOME!!<br><br>
-            <b>Visits:</b> {visits}<br><br>
-            Request served by server: {hostname}
-            """.format(visits=visits, hostname=socket.gethostname())
-    
+    html = """
+        <h3>Hello World!</h3>
+        <p>Here I am having fun with Pivotal Web Service!!</p>
+        <p><strong>Visits:</strong> {visits}</p>
+        <p>Request served by server: {hostname}</p>
+        """.format(
+        visits=visits, hostname=socket.gethostname()
+    )
+
     return html
 
+
 if __name__ == "__main__":
-    app.run(debug=False, host='0.0.0.0', port=int(os.getenv('PORT', '5000')))
+    app.run(debug=False, host="0.0.0.0", port=int(os.getenv("PORT", "5000")))
