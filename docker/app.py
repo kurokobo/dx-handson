d7df459bbe41da00e9b562ed14718a5cc5e575d7
kurokobo <kuro664@gmail.com>
2019-12-20 14:55:27 +0000

    Fix by linter

--- docker/app.py
+++ docker/app.py
@@ -1,12 +1,11 @@
 from flask import Flask
 from redis import Redis, RedisError
-import os
 import socket
 
 # Connect to Redis
 redis = Redis(host="redis", db=0, socket_connect_timeout=2, socket_timeout=2)
-
 app = Flask(__name__)
+
 
 @app.route("/")
 def helloworld():
@@ -15,12 +14,16 @@ def helloworld():
     except RedisError:
         visits = "<i>Cannot connect to Redis, counter disabled.</i>"
 
-    html = """<h3>Hello World!</h3><br>
-            Here I am having fun with Docker and Kubernetes!!<br><br>
-            <b>Visits:</b> {visits}<br><br>
-            Request served by server: {hostname}
-            """.format(visits=visits, hostname=socket.gethostname())
+    html = """
+        <h3>Hello World!</h3>
+        <p>Here I am having fun with Docker and Kubernetes!!</p>
+        <p><strong>Visits:</strong> {visits}</p>
+        <p>Request served by server: {hostname}</p>
+        """.format(
+        visits=visits, hostname=socket.gethostname()
+    )
     return html
 
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80)
+    app.run(host="0.0.0.0", port=80)
